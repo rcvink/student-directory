9dba13cd94ce5f0129cb5d92b2a44966560dfbe8
rcvink <rrvink@gmail.com>
2017-12-17 21:26:55 +0000

    Refactor interactive menu choices

--- directory.rb
+++ directory.rb
@@ -1,57 +1,62 @@
+@students = [] #an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will terminate the program
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will terminate the program
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def input_students
   puts "Please enter the names of the students, their cohort, and their place of birth"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
-  # get the cohort
+  # get the cohort, set to November if no input
   cohort = gets.chomp
   if cohort.empty?
     cohort = "November"
   end
   # get the country of birth
   birthplace = gets.chomp
-  # while the name is not empty, repeat this codes
+  # while the name or birthplace are not empty, repeat this:
   while !(name.empty? || birthplace.empty?) do
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, birthplace: birthplace.to_sym}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym, birthplace: birthplace.to_sym}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
     cohort = gets.chomp
     if cohort.empty?
       cohort = "November"
     end
-
     birthplace = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -59,19 +64,19 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  if students.length == 0
+def print_students_list
+  if @students.length == 0
     puts "There are no students at Villains Academy!"
   else
-    students.each_with_index do |student, index|
+    @students.each_with_index do |student, index|
       puts "#{index + 1}. name: #{student[:name].center(20)} | cohort: #{student[:cohort].to_s.center(12)} | country: #{student[:birthplace].to_s.center(15)}"
     end
   end
 end
 
-def print_footer(students)
-  footer_message = "Overall, we have #{students.count} great student"
-  if students.count > 1
+def print_footer
+  footer_message = "Overall, we have #{@students.count} great student"
+  if @students.count > 1
     puts footer_message + "s"
   else
     puts footer_message
